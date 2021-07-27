67483743f1fb8dd4d0fdb2b47309304acad848d7
TreeKR <johanbag290@gmail.com>
2021-07-27 18:55:54 +0900

    First commit

---
+++ src/console-noti.js
@@ -0,0 +1,21 @@
+dev =
+	"\
+1. 테마 변경 \n\
+    Ⅰ. theme 파라미터에 테마 이름값을 포함한 URL로 접속시 테마가 적용되어 로딩됩니다. \n\
+         사용 가능한 값으로는 basic, dark, dracula, light, ocean 가 있습니다. \n\
+         자세한 사항은 https://github.com/myoun/NYThemes 를 참고하세요. \n\
+\n\
+    Ⅱ. 콘솔에 changeTheme(themename) 함수를 실행해 테마 변경이 가능합니다.\n\
+         새로고침 없이 테마를 변경할 수 있습니다. \n\
+\n\
+2. Bot API \n\
+     https://github.com/WintChoco/nychat-js 비공식 라이브러리가 존재합니다. \n\
+     이것을 사용해서 발생한 일에 대해서는 일절 책임지지 않습니다. \n\
+\n\
+이 문서는 계속 업데이트 됩니다.\
+";
+console.log("%c잠깐만요!", "color: red; font-size: 4.5em; font-weight: bolder; text-shadow: black 4px 4px; font-family: 'Noto Sans KR';");
+console.log("%c이 기능은 개발자용으로 브라우저에서 제공되는 내용입니다.\n누군가 NYChat 기능을 악용하기 위해 콘텐츠를 복사하여 붙여넣으라고 했다면 사기 행위로 간주하세요.", "color: white; font-size: 1.6em; font-family: 'Noto Sans KR';");
+console.log("%c\n===========================================================\n", "color: white; font-size: 1.6em; font-weight: bolder; font-family: 'Noto Sans KR'; ");
+console.log("%c아래는 개발자를 위해 재공되는 기능들입니다.", "color: red; font-size: 1.8em; font-weight: bolder; text-shadow: black 2px 2px; font-family: 'Noto Sans KR';");
+console.log(`%c${dev}`, "color: white; font-size: 1.2em; font-family: 'Noto Sans KR';");
